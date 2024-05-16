f528fc3b033124ffc8be923b5bf624fe995f511f
Abdulahi Mohamed <abdulahim002@yahoo.com>
2024-05-15 20:18:12 -0400

    fixed code to be able to fetch api

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,22 @@
 const apiKey = '676ad8e513de1266b5f536d6cb1e0eac';
 const apiUrl = 'https://api.openweathermap.org/data/2.5/';
 
-function getWeather(city) {
-    const url = `${apiUrl}weather?q=${city}&units=metric&appid=${apiKey}`;
-}
+// function getWeather(city) {
+//     const url = `${apiUrl}weather?q=${city}&units=metric&appid=${apiKey}`;
+// }
 
-// funtion to fetch from weather API
-fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-        displayCurrentWeather(data);
-    })
-    .catch(error => {
-        console.error('Error fetching current weather:', error);
-        alert('Failed to fetch weather data. Please try again');
-    });
+// // funtion to fetch from weather API
+// fetch(apiUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         displayCurrentWeather(data);
+//     })
+//     .catch(error => {
+//         console.error('Error fetching current weather:', error);
+//         alert('Failed to fetch weather data. Please try again');
+//     });
 
 
-$(document).ready(function() {
-    $('#search-btn').click(function() {
-        const city = $('#city-input').val().trim();
-        if (city) {
-            getWeather(city);
-            getForecast(city);
-            addToHistory(city);
-        } else {
-            alert('Please enter a city name');
-        }
-        
-    });
-});
 
 function getWeather(city) {
     const url = `${apiUrl}weather?q=${city}&units=metric&appid=${apiKey}`;
@@ -46,8 +33,27 @@ function getWeather(city) {
     });
 }
 
-function getForecast(city) {
+function getCoords(city) {
     const url = `${apiUrl}weather?q=${city}&units=metric&appid=${apiKey}`;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            // set the lat and lon from the data to a variable and make a second api call
+            console.log(data)
+            getForecast(data.coord.lat, data.coord.lon)
+            //displayForecast(data);
+        },
+        error: function(error) {
+            console.error('Error fetching forecast:', error);
+            alert('Failed to retrieve forcast data.')
+        }
+    });
+}
+
+function getForecast(lat, lon) {
+    const url = `${apiUrl}forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
     $.ajax({
         url: url,
         type: 'GET',
@@ -57,7 +63,7 @@ function getForecast(city) {
         },
         error: function(error) {
             console.error('Error fetching forecast:', error);
-            alert('Failed to retrieve forcast data.')
+            alert('Failed to retrieve forecast data.');
         }
     });
 }
@@ -69,12 +75,13 @@ function displayCurrentWeather(data, city) {
         <p>Wind: ${data.wind.speed} MPH</p>
         <p>Humidity: ${data.main.humidity} %</p>
     `;
-    $('#current-weather').html(weatherHtml);
+    $('#weather-results').html(weatherHtml);
 }
 
 function displayForecast(data) {
     const forecast = document.getElementById('forecast-results');
     forecast.innerHTML = '<h2>5-Day Forecast</h2>';
+    console.log(data);
     data.list.forEach((item, index) => {
         if (index % 8 === 0) { 
             forecast.innerHTML += `<div>
@@ -86,7 +93,7 @@ function displayForecast(data) {
                                     </div>`;
         }
     })
-    $('#forecast-results').html(forecastHtml);
+   // $('#forecast-results').html(forecast);
 }
 
 function addToHistory(city) {
@@ -96,11 +103,23 @@ function addToHistory(city) {
         getWeather($(this).text());
         getForecast($(this).text());
     });
+    // implement local storage to save results
 }
-
-
-document.getElementById('search-btn').addEventListener('click', function() {
-    const city = document.getElementById('city-input').value;
-    getWeather(city);
-    getForecast(city);
+// document.getElementById('search-btn').addEventListener('click', function() {
+//     const city = document.getElementById('city-input').value;
+//     getWeather(city);
+//     getForecast(city);
+// });
+$(document).ready(function() {
+    $('#search-btn').click(function() {
+        const city = $('#city-input').val().trim();
+        if (city) {
+            getWeather(city);
+            getCoords(city);
+            addToHistory(city);
+        } else {
+            alert('Please enter a city name');
+        }
+        
+    });
 });
